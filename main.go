1b00238190224caebe452e4d8a47b35f2e096a52
yu ishiguro <yuuis.egr@gmail.com>
2019-09-16 11:25:18 +0900

    add initialization

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"github.com/yuuis/PersonalDataRepository/api"
+	"github.com/yuuis/PersonalDataRepository/infrastructures"
+	"github.com/yuuis/PersonalDataRepository/models"
+	"log"
+)
+
 func main() {
-	println("hello")
+	mysql, err := infrastructures.OpenMysql()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mysql.AutoMigrate(&models.Basic{}, &models.Location{})
+
+	s := infrastructures.NewServer()
+	api.Router(s)
+
+	if err := s.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
