7e5c650603b0bd13b14c472aaec88981d38f3936
yu ishiguro <yuuis.egr@gmail.com>
2019-09-16 14:53:38 +0900

    add controllers

--- api/controllers/basics_controller.go
+++ api/controllers/basics_controller.go
@@ -1,7 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/yuuis/PersonalDataRepository/api/presenters"
+	"github.com/yuuis/PersonalDataRepository/models/basic"
+	"net/http"
+)
 
-func GetBasics(c *gin.Context) {
-  // TODO: implementation
+func (r *Registry) GetBasics(c *gin.Context) {
+	ds := basic.NewDataStore(r.db)
+	basic, err := ds.Get()
+
+	if err != nil {
+		presenters.ViewInternalServerError(c, err)
+	}
+
+	presenters.JSON(c, http.StatusOK, basic)
 }
