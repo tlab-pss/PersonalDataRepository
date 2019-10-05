9da5853f9e2bdea1c16b59f45141fa13951d1dd9
yu ishiguro <yuuis.egr@gmail.com>
2019-10-05 19:33:52 +0900

    format

--- models/location/location.go
+++ models/location/location.go
@@ -3,8 +3,8 @@ package location
 import "time"
 
 type Location struct {
-	ID             string
-	Latitude       float64
-	Longitude      float64
-	CreatedAt      time.Time
+	ID        string
+	Latitude  float64
+	Longitude float64
+	CreatedAt time.Time
 }
