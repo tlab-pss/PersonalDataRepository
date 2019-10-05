8b9634b822d29614cfcb16e4669bdf99b9e90fa3
yu ishiguro <yuuis.egr@gmail.com>
2019-10-05 19:30:53 +0900

    add repositories for category

---
+++ models/small_category/small_category_repository.go
@@ -0,0 +1,50 @@
+package small_category
+
+import (
+	"context"
+	"github.com/yuuis/PersonalDataRepository/api/utilities"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type datastore struct {
+	col *mongo.Collection
+}
+
+func NewDataStore(c *mongo.Client) *datastore {
+	return &datastore{col: c.Database("pss").Collection("small_category")}
+}
+
+func (d *datastore) All() (*[]SmallCategory, error) {
+	// TODO: マスタデータを個人ごとに持っていいのか？
+
+	sc := make([]SmallCategory, 0)
+
+	h, err := d.col.Find(nil, bson.D{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := h.All(context.Background(), &sc); err != nil {
+		return nil, err
+	}
+
+	return &sc, nil
+}
+
+func (d *datastore) Find(id string) (*SmallCategory, error) {
+	sc := SmallCategory{}
+
+	findOptions := options.FindOne()
+	err := d.col.FindOne(nil, bson.M{"id": id}, findOptions).Decode(&sc)
+
+	if err == mongo.ErrNoDocuments {
+		return nil, utilities.NotFoundError
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &sc, nil
+}
