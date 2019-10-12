19e0568028f26177ef1d09be509c68d26e333563
yu ishiguro <yuuis.egr@gmail.com>
2019-10-12 12:35:32 +0900

    add small_category to intake

--- api/controllers/intake_controller.go
+++ api/controllers/intake_controller.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/yuuis/PersonalDataRepository/api/presenters"
 	"github.com/yuuis/PersonalDataRepository/api/utilities"
 	"github.com/yuuis/PersonalDataRepository/models"
 	"github.com/yuuis/PersonalDataRepository/models/plugins/hotpepper"
+	"github.com/yuuis/PersonalDataRepository/models/small_category"
 	"time"
 )
 
@@ -36,21 +38,33 @@ func (r *Registry) CreateIntake(c *gin.Context) {
 		presenters.ViewBadRequest(ctx, err)
 	}
 
-	i, err := ds.Store(&hotpepper.Intake{
-		ID:        models.GenerateUUID(),
-		Menu:      ipt.Menu,
-		Calorie:   ipt.Calorie,
-		CreatedAt: time.Now(),
-	})
-
+	_, err := small_category.NewDataStore(r.db).Find(ipt.SmallCategoryID)
 	if err != nil {
-		presenters.ViewInternalServerError(ctx, err)
-	}
+		switch err {
+		case utilities.NotFoundError:
+			presenters.ViewBadRequest(ctx, fmt.Errorf("small_category: %v", err))
+		default:
+			presenters.ViewInternalServerError(ctx, err)
+		}
+	} else {
+		i, err := ds.Store(&hotpepper.Intake{
+			ID:        models.GenerateUUID(),
+			Menu:      ipt.Menu,
+			Calorie:   ipt.Calorie,
+			SmallCategoryID: ipt.SmallCategoryID,
+			CreatedAt: time.Now(),
+		})
 
-	presenters.IntakeView(ctx, *i)
+		if err != nil {
+			presenters.ViewInternalServerError(ctx, err)
+		}
+
+		presenters.IntakeView(ctx, *i)
+	}
 }
 
 type inputIntake struct {
-	Menu    string
-	Calorie float64
+	Menu    string `json:"menu"`
+	Calorie float64 `json:"calorie""`
+	SmallCategoryID string `json:"small_category_id"`
 }
